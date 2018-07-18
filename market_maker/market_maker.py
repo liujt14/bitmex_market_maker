c0116a5c4ab26104405cc25ad9285736f5e0aef3
liujt14 <liujt14@gmail.com>
2018-07-18 19:58:25 +0800

    change market_maker.py

--- market_maker/market_maker.py
+++ market_maker/market_maker.py
@@ -518,6 +518,55 @@ class OrderManager:
         logger.info("Restarting the market maker...")
         os.execv(sys.executable, [sys.executable] + sys.argv)
 
+
+class CustomOrderManager(OrderManager):
+    """A sample order manager for implementing your own custom strategy"""
+
+    def place_orders(self) -> None:
+        # implement your custom strategy here
+        # my strategy
+        for i in range(0, 10):
+            i += 1
+            bitmex_usd = bitmex.BitMEX(base_url=settings.BASE_URL, symbol="XBTUSD",
+                                       apiKey=settings.API_KEY, apiSecret=settings.API_SECRET,
+                                       orderIDPrefix=settings.ORDERID_PREFIX, postOnly=settings.POST_ONLY,
+                                       timeout=settings.TIMEOUT)
+            bitmex_u18 = bitmex.BitMEX(base_url=settings.BASE_URL, symbol="XBTU18",
+                                       apiKey=settings.API_KEY, apiSecret=settings.API_SECRET,
+                                       orderIDPrefix=settings.ORDERID_PREFIX, postOnly=settings.POST_ONLY,
+                                       timeout=settings.TIMEOUT)
+            buy_orders = []
+            sell_orders = []
+            # get ticker data
+            # ticker data structure ticker = {
+            #         "last": instrument['lastPrice'],
+            #         "buy": bid,
+            #         "sell": ask,
+            #         "mid": (bid + ask) / 2
+            #     }
+            usd_ticker = bitmex_usd.ticker_data()
+            u18_ticker = bitmex_u18.ticker_data()
+            current_time = datetime.datetime.fromtimestamp(int(time.time()))
+            logger.info("datetime: %s, last price of XBTUSD is %.*f" % (current_time, usd_ticker['last']))
+            logger.info("last price of XBTU18 is %.*f" % u18_ticker['last'])
+            time.sleep(1)
+            usd_ticker_after = bitmex_usd.ticker_data()
+            u18_ticker_after = bitmex_u18.ticker_data()
+            after_datetime = datetime.datetime.fromtimestamp(int(time.time()))
+            logger.info("datetime: %s, last price of XBTUSD is %.*f" % (after_datetime, usd_ticker_after['last']))
+            logger.info("last price of XBTU18 is %.*f" % u18_ticker_after['last'])
+            delta_usd = (usd_ticker_after['last'] - usd_ticker['last']) * 100 / usd_ticker
+            delta_u18 = (u18_ticker_after['last'] - u18_ticker['last']) * 100 / u18_ticker
+            if (delta_usd - delta_u18) > 5:
+                buy_orders.append({'price': u18_ticker_after['sell'], 'orderQty': 100, 'side': "Buy"})
+
+            # populate buy and sell orders, e.g.
+            # buy_orders.append({'price': 999.0, 'orderQty': 100, 'side': "Buy"})
+            # sell_orders.append({'price': 1001.0, 'orderQty': 100, 'side': "Sell"})
+
+            self.converge_orders(buy_orders, sell_orders)
+
+
 #
 # Helpers
 #
@@ -537,12 +586,20 @@ def margin(instrument, quantity, price):
     return cost(instrument, quantity, price) * instrument["initMargin"]
 
 
-def run():
-    logger.info('BitMEX Market Maker Version: %s\n' % constants.VERSION)
+# def run():
+#     logger.info('BitMEX Market Maker Version: %s\n' % constants.VERSION)
+#
+#     om = OrderManager()
+#     # Try/except just keeps ctrl-c from printing an ugly stacktrace
+#     try:
+#         om.run_loop()
+#     except (KeyboardInterrupt, SystemExit):
+#         sys.exit()
+def run() -> None:
+    order_manager = CustomOrderManager()
 
-    om = OrderManager()
     # Try/except just keeps ctrl-c from printing an ugly stacktrace
     try:
-        om.run_loop()
+        order_manager.run_loop()
     except (KeyboardInterrupt, SystemExit):
         sys.exit()

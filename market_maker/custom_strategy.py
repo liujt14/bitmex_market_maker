ec1e35a2408c31b1b53e876e0815645e2ccbef62
liujt14 <liujt14@gmail.com>
2018-07-18 19:37:15 +0800

    add custom_strategy

--- market_maker/custom_strategy.py
+++ market_maker/custom_strategy.py
@@ -9,49 +9,53 @@ from market_maker.utils import log, constants, errors, math
 
 logger = log.setup_custom_logger('root')
 
+
 class CustomOrderManager(OrderManager):
     """A sample order manager for implementing your own custom strategy"""
 
     def place_orders(self) -> None:
         # implement your custom strategy here
         # my strategy
-        bitmex_usd = bitmex.BitMEX(base_url=settings.BASE_URL, symbol="XBTUSD",
-                                    apiKey=settings.API_KEY, apiSecret=settings.API_SECRET,
-                                    orderIDPrefix=settings.ORDERID_PREFIX, postOnly=settings.POST_ONLY,
-                                    timeout=settings.TIMEOUT)
-        bitmex_u18 = bitmex.BitMEX(base_url=settings.BASE_URL, symbol="XBTU18",
-                                     apiKey=settings.API_KEY, apiSecret=settings.API_SECRET,
-                                     orderIDPrefix=settings.ORDERID_PREFIX, postOnly=settings.POST_ONLY,
-                                     timeout=settings.TIMEOUT)
-        buy_orders = []
-        sell_orders = []
-        # get ticker data
-        # ticker data structure
-        # ticker = {
-        #         "last": instrument['lastPrice'],
-        #         "buy": bid,
-        #         "sell": ask,
-        #         "mid": (bid + ask) / 2
-        #     }
-        usd_ticker = bitmex_usd.ticker_data()
-        u18_ticker = bitmex_u18.ticker_data()
-        current_time = datetime.datetime.fromtimestamp(int(time.time()))
-        logger.info("datetime: %s, last price of XBTUSD is %.*f" % (current_time, usd_ticker['last']))
-        logger.info("last price of XBTU18 is %.*f" % u18_ticker['last'])
-        time.sleep(1)
-        usd_ticker_after = bitmex_usd.ticker_data()
-        u18_ticker_after = bitmex_u18.ticker_data()
-        after_datetime = datetime.datetime.fromtimestamp(int(time.time()))
-        logger.info("datetime: %s, last price of XBTUSD is %.*f" % (after_datetime, usd_ticker_after['last']))
-        logger.info("last price of XBTU18 is %.*f" % u18_ticker_after['last'])
-        delta_usd = (usd_ticker_after['last']-usd_ticker['last'])*100/usd_ticker
-        delta_u18 = (u18_ticker_after['last']-u18_ticker['last'])*100/u18_ticker
+        for i in range(0, 10):
+            i += 1
+            bitmex_usd = bitmex.BitMEX(base_url=settings.BASE_URL, symbol="XBTUSD",
+                                        apiKey=settings.API_KEY, apiSecret=settings.API_SECRET,
+                                        orderIDPrefix=settings.ORDERID_PREFIX, postOnly=settings.POST_ONLY,
+                                        timeout=settings.TIMEOUT)
+            bitmex_u18 = bitmex.BitMEX(base_url=settings.BASE_URL, symbol="XBTU18",
+                                         apiKey=settings.API_KEY, apiSecret=settings.API_SECRET,
+                                         orderIDPrefix=settings.ORDERID_PREFIX, postOnly=settings.POST_ONLY,
+                                         timeout=settings.TIMEOUT)
+            buy_orders = []
+            sell_orders = []
+            # get ticker data
+            # ticker data structure ticker = {
+            #         "last": instrument['lastPrice'],
+            #         "buy": bid,
+            #         "sell": ask,
+            #         "mid": (bid + ask) / 2
+            #     }
+            usd_ticker = bitmex_usd.ticker_data()
+            u18_ticker = bitmex_u18.ticker_data()
+            current_time = datetime.datetime.fromtimestamp(int(time.time()))
+            logger.info("datetime: %s, last price of XBTUSD is %.*f" % (current_time, usd_ticker['last']))
+            logger.info("last price of XBTU18 is %.*f" % u18_ticker['last'])
+            time.sleep(1)
+            usd_ticker_after = bitmex_usd.ticker_data()
+            u18_ticker_after = bitmex_u18.ticker_data()
+            after_datetime = datetime.datetime.fromtimestamp(int(time.time()))
+            logger.info("datetime: %s, last price of XBTUSD is %.*f" % (after_datetime, usd_ticker_after['last']))
+            logger.info("last price of XBTU18 is %.*f" % u18_ticker_after['last'])
+            delta_usd = (usd_ticker_after['last']-usd_ticker['last'])*100/usd_ticker
+            delta_u18 = (u18_ticker_after['last']-u18_ticker['last'])*100/u18_ticker
+            if (delta_usd-delta_u18) > 5:
+                buy_orders.append({'price': u18_ticker_after['sell'], 'orderQty': 100, 'side': "Buy"})
 
-        # populate buy and sell orders, e.g.
-        # buy_orders.append({'price': 999.0, 'orderQty': 100, 'side': "Buy"})
-        # sell_orders.append({'price': 1001.0, 'orderQty': 100, 'side': "Sell"})
+            # populate buy and sell orders, e.g.
+            # buy_orders.append({'price': 999.0, 'orderQty': 100, 'side': "Buy"})
+            # sell_orders.append({'price': 1001.0, 'orderQty': 100, 'side': "Sell"})
 
-        self.converge_orders(buy_orders, sell_orders)
+            self.converge_orders(buy_orders, sell_orders)
 
 
 def run() -> None:
